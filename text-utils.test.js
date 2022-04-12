a37acecdc69fe1aa7987eabfe1973c8053caf89c
Boris <boris@randebrock.com>
2022-04-13 00:24:11 +0200

    testing page upload

--- text-utils.test.js
+++ text-utils.test.js
@@ -1,15 +1,6 @@
-// Escribe una función llamada strictEquals(a, b) que devuelva el mismo valor que a === b.
-//Tu implementación no puede usar los operadores === ni !==.
-
-// Testea la función con este set de pruebas:
-
-// Dada la función strictEquals
-// Cuando se comparan <A> y <B>
-// Entonces el resultado será <Result>
-
 const strictEquals = (a, b) => Object.is(a, b);
 
-describe("given the stricktEqualsFunction", () => {
+describe("given the stricktEquals Function", () => {
   describe("when it receives values 1, 1", () => {
     test("it should return true", () => {
       const a = 1;
@@ -18,7 +9,7 @@ describe("given the stricktEqualsFunction", () => {
 
       const result = strictEquals(a, b);
 
-      expect(result).toBe(escxpectedResult);
+      expect(result).toBe(expectedResult);
     });
   });
 });
