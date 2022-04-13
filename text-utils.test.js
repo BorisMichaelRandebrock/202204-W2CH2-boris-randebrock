20c63ed28d4ed837bca23d4160b2cee79b408ecb
Boris <boris@randebrock.com>
2022-04-13 09:04:08 +0200

    first test

--- text-utils.test.js
+++ text-utils.test.js
@@ -1,11 +1,28 @@
-const strictEquals = (a, b) => Object.is(a, b);
+const strictEquals = (a, b) => {
+  Object.is(a, b);
+if (a = NaN && b = NaN ) {
+  return false;
+};
+};
 
-describe("given the stricktEquals Function", () => {
-  describe("when it receives values 1, 1", () => {
-    test("it should return true", () => {
+
+describe("Given the strictEquals function", () => {
+  describe("When it receives 1, 1", () => {
+    test("Then it should return true", () => {
       const a = 1;
       const b = 1;
       const expectedResult = true;
+
+      const result = strictEquals(a, b);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+  describe("When it receives NaN, Nan ", () => {
+    test("Then it should return false", () => {
+      const a = NaN;
+      const b = NaN;
+      const expectedResult = false;
 
       const result = strictEquals(a, b);
 
